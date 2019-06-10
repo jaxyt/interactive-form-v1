215b0f398d5c78439dd213fd3f87bfc2a9b57efd
jax <jax.maximillion.taylor@gmail.com>
2019-06-10 19:42:00 -0400

    fixed variable names

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-const form = $('form');
-const name = $('#name');
-const email = $('#mail');
-const jobRole = $('#title');
-const jobRoles = $('#title').children();
-const otherJob = $('#other');
-const tshirtSizes = $('#size');
-const tshirtDesigns = $('#design');
-const tshirtColors = $('#color');
-const activities = $('fieldset.activities').children('label');
-const paymentMethods = $('#payment').children();
-const creditCard = $('#credit-card');
-const ccFields = creditCard.children();
-const ccNumber = $('#cc-num');
-const zipCode = $('#zip');
-const cvvNumber = $('#cvv');
-const paypal = creditCard.next();
-const bitcoin = paypal.next();
-const submit = $('button[type="submit"]');
+const $form = $('form');
+const $name = $('#name');
+const $email = $('#mail');
+const $jobRole = $('#title');
+const $jobRoles = $('#title').children();
+const $otherJob = $('#other');
+const $tshirtSizes = $('#size');
+const $tshirtDesigns = $('#design');
+const $tshirtColors = $('#color');
+const $activities = $('fieldset.activities').children('label');
+const $paymentMethods = $('#payment').children();
+const $creditCard = $('#credit-card');
+const $ccFields = $creditCard.children();
+const $ccNumber = $('#cc-num');
+const $zipCode = $('#zip');
+const $cvvNumber = $('#cvv');
+const $paypal = $creditCard.next();
+const $bitcoin = $paypal.next();
+const $submit = $('button[type="submit"]');
 
 function isValidName(name) {
     const regex = /^[a-zA-Z]+ [a-zA-Z]+$/;
@@ -30,21 +30,17 @@ function isValidEmail(email) {
 
 function checkJob(job) {
     if(job === 'other'){
-        otherJob.show();
+        $otherJob.show();
     } else {
-        otherJob.hide();
+        $otherJob.hide();
     }
     return true;
 };
 
-function shirtSize(size) {
-    return true;
-};
 
 function checkShirtDesign(theme) {
     if (theme === 'js puns') {
-        tshirtColors.children().each(function (index) {
-            console.log($(this).text());
+        $tshirtColors.children().each(function (index) {
             if (/\(JS Puns shirt only\)/g.test($(this).text())) {
                 $(this).show();
             } else {
@@ -52,8 +48,7 @@ function checkShirtDesign(theme) {
             }
         })
     } else if (theme === 'heart js') {
-        tshirtColors.children().each(function (index) {
-            console.log($(this).text());
+        $tshirtColors.children().each(function (index) {
             if (/\(JS Puns shirt only\)/g.test($(this).text())) {
                 $(this).hide();
             } else {
@@ -64,19 +59,29 @@ function checkShirtDesign(theme) {
     return true;
 }
 
-function changeShirtColors(design, colors) {
-    return true;
-}
-
-function checkShirtColor(color) {
-    return true;
-};
-
-// function disableActivities(conflictingActivity) {
-    
-// };
-
 function checkActivityDates(activity) {
+    const $activityName = $(activity).attr('name');
+    const isChecked = activity.checked;
+    const regex = /([a-zA-Z]+) (\d\d?[a-z][a-z]-\d\d?[a-z][a-z])/g;
+    if ($activityName !== 'all') {
+        const $activityTime = $(activity).parent('label').text().match(regex)[0];
+        console.log($activityName);
+        console.log($activityTime);
+        $activities.each(function(index) {
+            const $conflict = $(this);
+            const $conflictName = $conflict.children().first().attr('name');
+            if ($conflictName !== 'all' && $conflictName !== $activityName) {
+                const $conflictTime = $conflict.text().match(regex)[0];
+                if ($activityTime === $conflictTime) {
+                    console.log(`${$conflictName} conflicts with ${$activityName}!!`)
+                } else {
+                    console.log(`${$conflictName} is available.`)
+                }
+            }
+        });
+    } else {
+        console.log('Main Conference');
+    }
     return true;
 };
 
@@ -100,69 +105,48 @@ function checkSubmissionComplete() {
     return true;
 }
 
-name.trigger('focus');
-otherJob.hide();
-creditCard.hide();
-paypal.hide();
-bitcoin.hide();
+$name.trigger('focus');
+$otherJob.hide();
+$creditCard.hide();
+$paypal.hide();
+$bitcoin.hide();
 
-name.focusout((e)=>{
+$name.focusout((e)=>{
     console.log(isValidName(e.target.value));
 });
 
-email.focusout((e)=>{
+$email.focusout((e)=>{
     console.log(isValidEmail(e.target.value));
 });
 
-jobRole.change((e)=>{
+$jobRole.change((e)=>{
     console.log(checkJob(e.target.value));
 });
 
-tshirtSizes.change((e)=>{
-    console.log(shirtSize(e.target.value));
-});
 
-tshirtDesigns.change((e)=>{
+$tshirtDesigns.change((e)=>{
     console.log(checkShirtDesign(e.target.value)); 
 });
 
-tshirtColors.change((e)=>{
-    console.log(checkShirtColor(e.target.value));
+
+$activities.on('click', (e)=>{
+    console.log(e.target);
+    // console.log(e.target.checked);
+    // console.log(e.target.parentElement.textContent);
+    console.log(checkActivityDates(e.target));
 });
 
-activities.on('click', (e)=>{
-    console.log(checkActivityDates(e.target.value));
-});
-
-paymentMethods.on('click', (e)=>{
+$paymentMethods.on('click', (e)=>{
     console.log(checkPaymentMethod(e.target.value));
 });
 
-ccNumber.focusout((e)=>{
+$ccNumber.focusout((e)=>{
     console.log(isValidCCNumber(e.target.value));
 });
 
-submit.on('click', (e)=>{
+$submit.on('click', (e)=>{
     e.preventDefault();
     console.log(checkSubmissionComplete());
 });
 
 
-// console.log(form);
-// console.log(name);
-// console.log(email);
-// console.log(jobRoles);
-// console.log(otherJob);
-// console.log(tshirtSizes);
-// console.log(tshirtDesigns);
-// console.log(tshirtColors);
-// console.log(activities);
-// console.log(paymentMethods);
-// console.log(creditCard);
-// console.log(ccFields);
-// console.log(ccNumber);
-// console.log(zipCode);
-// console.log(cvvNumber);
-// console.log(paypal);
-// console.log(bitcoin);
-// console.log(submit);
